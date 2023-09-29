050ead7f0c30bcffefd82627b6014fb96d3c50e6
mehmetraufoguz <88249309+mehmetraufoguz@users.noreply.github.com>
2023-09-29 13:56:27 +0300

    update rename geth to geth_linux

--- geth/run_linux.sh
+++ geth/run_linux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ ! -d "/home/services/geth/chain_data/geth" ]; then
     echo "Initializing directory..."
-    ./geth --datadir /home/services/geth/chain_data init /home/services/geth/genesis.json
+    ./geth_linux --datadir /home/services/geth/chain_data init /home/services/geth/genesis.json
 fi
 
-    ./geth\
+    ./geth_linux\
             --datadir /home/services/geth/chain_data\
             --config /home/services/geth/config.toml\
             --cache 8192
